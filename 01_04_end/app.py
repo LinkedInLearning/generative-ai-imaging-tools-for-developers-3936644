0a334f4f2ddf72e504acdd6d7d42a34a397f769f
Ronnie Sheer <ronysheer@gmail.com>
2024-09-12 10:30:08 +0300

    initial functionality'

--- 01_04_end/app.py
+++ 01_04_end/app.py
@@ -1,3 +1,4 @@
+from collections import deque
 from flask import Flask, request, jsonify, render_template
 
 from openai import OpenAI
@@ -5,6 +6,58 @@ from openai import OpenAI
 app = Flask(__name__)
 
 client = OpenAI()
+
+image_queue = deque()
+
+
+def moderate_prompt(client, prompt):
+    """
+    Moderate the prompt
+    """
+    moderation = client.moderations.create(input=prompt)
+    flagged = moderation.results[0].flagged
+    if flagged:
+        return "Your prompt has been flagged for moderation. Please try another prompt."
+    return prompt
+
+
+def enhance_prompt(client, prompt):
+    """
+    Enhance the prompt by adding a few more tokens
+    """
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "You take image-generation prompts and turn them into sticker styled image-generation prompts.",
+                    }
+                ],
+            },
+            {"role": "user", "content": [{"type": "text", "text": "A cat"}]},
+            {
+                "role": "assistant",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "A cute, cartoon-style cat with big eyes, wearing a colorful collar, surrounded by playful yarn balls and bright daisies, in a vibrant sticker design.",
+                    }
+                ],
+            },
+            {"role": "user", "content": [{"type": "text", "text": prompt}]},
+        ],
+        temperature=1,
+        max_tokens=2048,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        response_format={"type": "text"},
+    )
+    return response.choices[0].message.content
 
 
 @app.get("/")
@@ -19,14 +72,29 @@ def api():
     """
     data = request.json
     prompt = data["prompt"]
+    moderation = client.moderations.create(input=prompt)
+
+    flagged = moderation.results[0].flagged
+    if flagged:
+        return jsonify({"error": prompt})
+    proccessed_prompt = enhance_prompt(client, prompt)
     response = client.images.generate(
         model="dall-e-3",
-        prompt=prompt,
+        prompt=proccessed_prompt,
         size="1024x1024",
         quality="standard",
         n=1,
     )
     image_url = response.data[0].url
-    return jsonify({"image_url": image_url})
+    image_queue.append({"prompt": prompt, "url": image_url})
+    return jsonify({"url": image_url})
+
+
+@app.get("/api/list/")
+def get_image():
+    if image_queue:
+        return jsonify(list(image_queue))
+    return []
+
 
 app.run(debug=True)
