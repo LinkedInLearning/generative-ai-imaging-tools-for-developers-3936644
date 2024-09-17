cf44d9652ef539ea85b56744217f5525f2268c52
Ronnie Sheer <ronysheer@gmail.com>
2024-09-17 14:32:00 +0300

    01_04

---
+++ 01_04/app.py
@@ -0,0 +1,67 @@
+from collections import deque
+from PIL import Image
+import os
+import uuid
+
+from flask import Flask, request, jsonify, render_template
+import requests
+from openai import OpenAI
+
+app = Flask(__name__)
+
+client = OpenAI()
+
+image_queue = deque()
+
+def enhance_prompt(prompt):
+    style = "cartoon style"
+    return f"{prompt} {style}"
+
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+
+@app.post("/api/")
+def api():
+    """
+    payload includes prompt, returns image
+    """
+    data = request.json
+    prompt = data["prompt"]
+
+    proccessed_prompt = enhance_prompt(client, prompt)
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=proccessed_prompt,
+        size="1024x1024",
+        quality="standard", 
+        n=1,
+    )
+    url = load_image(response.data[0].url, prompt)
+    return jsonify({"url": url})
+
+
+@app.get("/api/list/")
+def get_image():
+    return jsonify([
+        {"prompt": image.split("-id")[0], "url": f"/static/images/{image}"}
+        for image in os.listdir("static/images")
+        if image.endswith(".png")
+    ])
+    
+
+def load_image(url, prompt):
+    """
+    Load the image
+    """
+    response = requests.get(url, stream=True)
+    image = Image.open(response.raw)
+    uuid_string = str(uuid.uuid4())[:3]
+    file_path = f"static/images/{prompt}-id{uuid_string}.png"
+    resized = image.resize((512, 512,))
+    image.save(file_path)
+    return f"/{file_path}"
+    
+app.run(debug=True, port=5000)
