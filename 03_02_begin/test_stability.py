9fe5c00d91db864700218e4a752b48a0616fcc1d
Ronnie Sheer <ronysheer@gmail.com>
2024-09-26 11:42:59 +0300

    03_02

---
+++ 03_02_begin/test_stability.py
@@ -0,0 +1,6 @@
+import os
+import requests
+
+STABILITY_API_KEY = os.getenv("STABILITY_API_KEY") # make sure to set this as an environment variable
+
+USER_PROMPT = "Vintage photo of a beautiful sunset over the ocean."
